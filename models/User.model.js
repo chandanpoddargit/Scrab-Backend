336ffb387325caff1927330e1391edfd667be0c1
chandanpoddargit <160326877+chandanpoddargit@users.noreply.github.com>
2024-09-05 20:41:22 +0530

    Update User.model.js

--- models/User.model.js
+++ models/User.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      required: [true, "Please enter an email"],
+      required: [true,  "Please enter an email"],
       unique: true,
       lowercase: true,
       validate: [isEmail, "Please enter a valid email"],
